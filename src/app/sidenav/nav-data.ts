5200ae84bb6a8539d4689212055a560413bad25e
vaibhavkwagh <vaibhavkw2001@gmail.com>
2024-04-26 12:12:01 +0530

    Initial commit

---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,37 @@
+export const navbarData = [
+    {
+        routeLink: 'dashboard',
+        icon: 'fal fa-home',
+        label: 'Dashboard'
+    },
+    {
+        routeLink: 'products',
+        icon: 'fal fa-box-open',
+        label: 'Teachers Data'
+    },
+    {
+        routeLink: 'statistics',
+        icon: 'fal fa-chart-bar',
+        label: 'Blogs'
+    },
+    {
+        routeLink: 'coupens',
+        icon: 'fal fa-tags',
+        label: 'Coupens'
+    },
+    {
+        routeLink: 'pages',
+        icon: 'fal fa-file',
+        label: 'Pages'
+    },
+    {
+        routeLink: 'media',
+        icon: 'fal fa-camera',
+        label: 'Media'
+    },
+    {
+        routeLink: 'settings',
+        icon: 'fal fa-cog',
+        label: 'Settings'
+    },
+];
